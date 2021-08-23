7777d52d27d3e9fb72c3e4c4c1c1205627f687bc
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-08-23 13:34:48 +0545

    comple blog section with delete and edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
 
+  #home page
+  #show all posts
     def index
         @posts = Post.all.order("created_at DESC")
     end
 
+    #show specific post
     def show
         @post = Post.find(params[:id])
     end
 
+    #add new post
     def new
         @post = Post.new
     end
@@ -22,16 +26,32 @@ class PostsController < ApplicationController
         end
 
     end
-  
+
+    #update a posts
     def update
+        @post =Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+
+    #delete a post
     def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+
+      redirect_to root_path
     end
 
     private
-
-
     def post_params
         params.require(:post).permit(:title, :content)
     end
